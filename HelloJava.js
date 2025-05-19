57e662a7eab7509def97720cb040dbab38b12753
mohamedhamdy9966 <mh7008002@gmail.com>
2025-05-19 14:10:33 +0300

    added

--- HelloJava.js
+++ HelloJava.js
@@ -4,12 +4,17 @@ class HelloJava{
         console.log("Hello, World!");
     }
     printSprinkles() {
-        console.log("new");   
+        console.log("new"); 
+        console.log("new"); 
+        console.log("new"); 
     }
 }
 
 const hello = new HelloJava();
 const hello2 = new HelloJava();
 const hello3 = new HelloJava();
+const hello4 = new HelloJava();
+const hello5 = new HelloJava();
+const hello6 = new HelloJava();
 hello.printHelloWorld();
 hello.printSprinkles()
